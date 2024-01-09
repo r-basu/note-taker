9411eb17a276c8f7558149c99bbd1e5d57ae5c52
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-09 18:19:43 -0500

    Add fsUtils helper from exercises to read JSON data db and append

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
+const { readFromFile, readAndAppend } = require("../../helpers/fsUtils");
 
 router.get("/", async (req, res) => {
   try {
-
-    res.status(200).json(xxdData);
+    readFromFile("./db/db.json").then((data) =>
+      res.status(200).json(JSON.parse(data))
+    );
   } catch (err) {
     res.status(500).json(err);
   }
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/feedback', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
