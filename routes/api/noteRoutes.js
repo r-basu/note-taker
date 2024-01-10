85ee337f2faa0e594e134ec4fd13eb2e1e129c49
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-09 23:46:06 -0500

    add delete route with fs sync

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
-const { readFromFile, readAndAppend } = require("../../helpers/fsUtils");
-const uuid = require('../../helpers/uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../../helpers/fsUtils");
+const uuid = require("../../helpers/uuid");
+const fs = require("fs");
 
 router.get("/", async (req, res) => {
   try {
-    readFromFile("./db/db.json").then((data) =>
-      res.status(200).json(JSON.parse(data))
+    readFromFile("./db/db.json").then((notes) =>
+      res.status(200).json(JSON.parse(notes))
     );
   } catch (err) {
     res.status(500).json(err);
@@ -14,19 +19,35 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
 
-    const {title, text} = req.body
+    const { title, text } = req.body;
     if (req.body) {
       const newNote = {
         title,
         text,
-        id: uuid()
-      }
-      readAndAppend(newNote, "./db/db.json")
-      res.status(200).json(`Note added successfully`)
+        id: uuid(),
+      };
+      readAndAppend(newNote, "./db/db.json");
+      res.status(200).json(`Note added successfully`);
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = fs.readFileSync("./db/db.json", "utf8");
+    const notes = JSON.parse(data);
+
+    const updatedNotes = notes.filter((note) => note.id !== id);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(updatedNotes));
+
+    res.status(200).json({ message: "Note deleted successfully" });
   } catch (err) {
     res.status(500).json(err);
   }
